20443a95f8317172fc2d682425bfe37bd8d7c177
Noel <noel.osagie@devoteam.com>
2024-04-23 00:59:24 +0100

    add aws provider and create s3 bucket

---
+++ buckets.tf
@@ -0,0 +1,8 @@
+resource "aws_s3_bucket" "acme-ops-data" {
+  bucket = "nlosg-acme-corp-ops"
+
+  tags = {
+    Name        = "ACME bucket"
+    Environment = "Prod"
+  }
+}
--- provider.tf
+++ provider.tf
@@ -1,3 +1,17 @@
 provider "google" {
   project = "nlosg-acme-corp-ops"
 }
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+
+provider "aws" {
+  region = "eu-west-2"
+}
